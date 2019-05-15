e05fa939f8c4026f4397a5242f66f2b1b4ae1867
quansenB <inaki.breinbauer@gmail.com>
2019-05-15 16:06:46 +0200

    article finish

--- Article/Article.js
+++ Article/Article.js
@@ -3,18 +3,23 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
+    console.log("text");
+
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = domElement.querySelector(".expandButton");
+
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = "expand";
+
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener("click", this.expandArticle.bind(this));
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    console.log("test");
+    this.domElement.classList.toggle("article-open");
   }
 }
 
@@ -26,4 +31,8 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll(".article");
+
+articles.forEach((item) => {
+  return new Article(item);
+});
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,17 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
+  if (menu.style.display === "none") {
+    menu.style.display = "block";
+  }
+  else {
+    menu.style.display = "none";
+  }
 }
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector(".menu");
+
 
 // create a reference to the ".menu-button" class
 const menuButton= document.querySelector(".menu-button");
@@ -12,6 +19,10 @@ const menuButton= document.querySelector(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
 
 
-menuButton.addEventListener("click", (event) =>{
-  event.target.style.dissplay = "block";
-})
+menuButton.addEventListener("click", toggleMenu());
+  /* menu.style.display = "block";
+}) */
+
+/* menu.addEventListener("mouseleave", (event) =>{
+  menu.style.display = "none";
+}) */
